325e117f4e2560a013511a9ccc74b8fa8c1a9272
Maxim Pekarsky <flufci@gmail.com>
2016-04-28 00:17:56 -0400

    building out filling shifts

--- app/models/advisor.rb
+++ app/models/advisor.rb
@@ -1,3 +1,28 @@
 class Advisor < ActiveRecord::Base
 	belongs_to :shift
+
+	def self.is_off?(shift, advisor)
+		@vacations = Vacation.where(name: advisor.name)				#pull all the time off for this advisor
+
+		start_valid = true;
+		end_valid = true;
+
+		@vacations.each do |v|										#for each time off
+
+			puts "testing one!"
+
+			start_valid = false if shift.start.between?(v.start, v.end)			#is the start of the shift during a time off?
+			end_valid = false if shift.end.between?(v.start, v.end)				# is the end of the shift during a time off?
+		end
+
+		if !start_valid || !end_valid
+			puts "start overlaps"
+			return true
+		else
+			puts "end overlaps"
+			return false
+		end	
+	end
+
+
 end
--- app/models/shift.rb
+++ app/models/shift.rb
@@ -1,2 +1,20 @@
 class Shift < ActiveRecord::Base
+
+
+	def self.fill_shift(shift)
+
+			
+			#pick random advisor
+			rand_num = Advisor.count
+			rand_advisor = Advisor.find(Random.rand(rand_num) + 4)
+			puts "Found #{rand_advisor.name}"
+			# is this advisor available?
+			Advisor.is_off?(shift, rand_advisor)
+
+
+
+	end
+
+
+
 end
